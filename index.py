6cb786c9b61fa5ec76643baeecb683fc726dc776
Adrian <adrian.grosch@singular.inf.br>
2023-12-23 22:34:15 -0300

    adicionada pasta de media

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 from pytube import YouTube
 from moviepy.editor import *
-
+import os
 url = str(input("Enter the URL of the video you want to download: \n>>"))
 yt_video = YouTube(url)
 stream = yt_video.streams.get_highest_resolution()
-downloaded_file = stream.download()
+downloaded_file = stream.download('media')
 video = VideoFileClip(downloaded_file)
 audio = video.audio
-audio.write_audiofile(f'{yt_video.title}.mp3')
+audio.write_audiofile(f'./media/{yt_video.title}.mp3')
